a556c80543707e4bda7e73e7a83da131e7e8be5d
Douglas Muth <dmuth@dmuth.org>
2019-04-16 23:41:42 -0400

    Oops--forgot to tag dmuth1/splunk-lab-ml

--- bin/devel.sh
+++ bin/devel.sh
@@ -15,6 +15,7 @@ echo "# "
 echo "# Tagging container..."
 echo "# "
 docker tag splunk-lab dmuth1/splunk-lab
+docker tag splunk-lab-ml dmuth1/splunk-lab-ml
 
 SPLUNK_DEVEL=1 SPLUNK_BG=0 REST_KEY=${REST_KEY} SPLUNK_PASSWORD=password1 ./go.sh
 
