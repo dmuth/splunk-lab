c943141c9ec64e76127f1aa9a28f3d381f4e444a
Douglas Muth <douglas_muth@cable.comcast.com>
2021-02-04 16:38:06 -0500

    Added support for NO_BUILD

--- bin/devel.sh
+++ bin/devel.sh
@@ -12,13 +12,26 @@ BUILD="${CACHE}/build"
 pushd $(dirname $0) > /dev/null
 cd ..
 
-./bin/build.sh $@
+#
+# Skip our building if NO_BUILD is set.
+#
+if test "$NO_BUILD"
+then
+	echo "# "
+	echo "# Skipping build due to \$NO_BUILD specified..."
+	echo "# "
 
-echo "# "
-echo "# Tagging container..."
-echo "# "
-docker tag splunk-lab dmuth1/splunk-lab
-docker tag splunk-lab-ml dmuth1/splunk-lab-ml
+else
+	./bin/build.sh $@
+
+	echo "# "
+	echo "# Tagging container..."
+	echo "# "
+	docker tag splunk-lab dmuth1/splunk-lab
+	docker tag splunk-lab-ml dmuth1/splunk-lab-ml
+
+fi
+
 
 SPLUNK_DEVEL=1 REST_KEY=${REST_KEY} SPLUNK_PASSWORD=${SPLUNK_PASSWORD:-password1} ./go.sh
 
