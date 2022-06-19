a29a01f35ea70eb54bb86de38de7d887f00f59e1
Douglas Muth <dmuth@dmuth.org>
2022-06-18 22:46:34 -0400

    Added tagging

--- bin/push.sh
+++ bin/push.sh
@@ -16,9 +16,10 @@ cd ..
 function print_syntax() {
 
 	echo "! "
-	echo "! Syntax: $0 VERSION"
+	echo "! Syntax: $0 MAJOR MINOR"
 	echo "! "
-	echo "! VERSION - Major version number of Splunk, to be used tagging Docker containers"
+	echo "! MAJOR - Major version number of Splunk, to be used tagging Docker containers (e.g. 8)"
+	echo "! MINOR - Minor version number of Splunk (e.g. 8.2.6)"
 	echo "! "
 	exit 1
 
@@ -29,19 +30,29 @@ if test "$1" == "-h" -o "$1" == "--help"
 then
 	print_syntax
 
-elif test ! "$1"
+elif test ! "$2"
 then
 	print_syntax
 
 fi
 
-VERSION=$1
+MAJOR=$1
+MINOR=$2
 
 re="^[0-9]+$"
-if ! [[ "${VERSION}" =~ ${re} ]]
+if ! [[ "${MAJOR}" =~ ${re} ]]
 then
 	echo "! "
-	echo "! $0: Version ${VERSION} is not a number!"
+	echo "! $0: Version ${MAJOR} is not a number!"
+	echo "! "
+	exit 1
+fi
+
+re="^[0-9\.]+$"
+if ! [[ "${MINOR}" =~ ${re} ]]
+then
+	echo "! "
+	echo "! $0: Version ${MINOR} is not a number!"
 	echo "! "
 	exit 1
 fi
@@ -51,17 +62,21 @@ echo "# "
 echo "# Tagging containers..."
 echo "# "
 docker tag splunk-lab dmuth1/splunk-lab
-docker tag splunk-lab dmuth1/splunk-lab:${VERSION}
+docker tag splunk-lab dmuth1/splunk-lab:${MAJOR}
+docker tag splunk-lab dmuth1/splunk-lab:${MINOR}
 docker tag splunk-lab-ml dmuth1/splunk-lab-ml
-docker tag splunk-lab-ml dmuth1/splunk-lab-ml:${VERSION}
+docker tag splunk-lab-ml dmuth1/splunk-lab-ml:${MAJOR}
+docker tag splunk-lab-ml dmuth1/splunk-lab-ml:${MINOR}
 
 echo "# "
 echo "# Pushing containers to Docker Hub..."
 echo "# "
 docker push dmuth1/splunk-lab
-docker push dmuth1/splunk-lab:${VERSION}
+docker push dmuth1/splunk-lab:${MAJOR}
+docker push dmuth1/splunk-lab:${MINOR}
 docker push dmuth1/splunk-lab-ml
-docker push dmuth1/splunk-lab-ml:${VERSION}
+docker push dmuth1/splunk-lab-ml:${MAJOR}
+docker push dmuth1/splunk-lab-ml:${MINOR}
 
 
 echo "# Done!"
